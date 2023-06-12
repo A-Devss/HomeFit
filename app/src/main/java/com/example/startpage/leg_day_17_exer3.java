30e8ef528f1ffd282412d20ac198643827d43590
Adner <adnerdevila23@gmail.com>
2023-06-12 19:29:15 +0800

    june 12 legs

--- app/src/main/java/com/example/startpage/leg_day_17_exer3.java
+++ app/src/main/java/com/example/startpage/leg_day_17_exer3.java
@@ -2,13 +2,55 @@ package com.example.startpage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class leg_day_17_exer3 extends AppCompatActivity {
+public class leg_day_17_exer3 extends AppCompatActivity implements View.OnClickListener{
+    private Button btn_next;
+    private TextView btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leg_day17_exer3);
+        getSupportActionBar().setTitle("Day 17");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        btn_next = findViewById(R.id.leg_d17_btn_next3);
+        btn_next.setOnClickListener(this);
+        btnBack = findViewById(R.id.leg_d17_btn_back3);
+        btnBack.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        if(v == btnBack){
+            startActivity(new Intent(leg_day_17_exer3.this, leg_day_17_exer2.class));
+            overridePendingTransition(0, 0);
+        }
+        else if(v == btn_next){
+            startActivity(new Intent(leg_day_17_exer3.this, leg_day_17_exer4.class));
+            overridePendingTransition(0, 0);
+        }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            // Handle the back button click here or leave it empty to cancel the default animation
+            startActivity(new Intent(leg_day_17_exer3.this, leg_day_17_exer2.class));
+            overridePendingTransition(0, 0);
+            return true; // Return true to indicate that the event has been handled
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(0, 0);
     }
 }
--- app/src/main/java/com/example/startpage/leg_day_27_exer5.java
+++ app/src/main/java/com/example/startpage/leg_day_27_exer5.java
@@ -2,13 +2,57 @@ package com.example.startpage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class leg_day_27_exer5 extends AppCompatActivity {
+import java.util.Objects;
+
+public class leg_day_27_exer5 extends AppCompatActivity implements View.OnClickListener{
+    private Button btnFinish;
+    private TextView btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leg_day27_exer5);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Day 27");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        btnFinish = findViewById(R.id.leg_d27_btn_finish);
+        btnFinish.setOnClickListener(this);
+        btnBack = findViewById(R.id.leg_d27_btn_back5);
+        btnBack.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        if(v == btnBack){
+            startActivity(new Intent(leg_day_27_exer5.this, leg_day_27_exer4.class));
+            overridePendingTransition(0, 0);
+        }
+        else if(v == btnFinish){
+            startActivity(new Intent(leg_day_27_exer5.this, legs_page.class));
+            overridePendingTransition(0, 0);
+        }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            // Handle the back button click here or leave it empty to cancel the default animation
+            startActivity(new Intent(leg_day_27_exer5.this, leg_day_27_exer4.class));
+            overridePendingTransition(0, 0);
+            return true; // Return true to indicate that the event has been handled
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(0, 0);
     }
 }
